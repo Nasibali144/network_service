aadeecd53823fd3624b56e72ad695877bd29904e
User <abdiyevnasibali>
2022-04-25 17:07:52 +0500

    initial commit

--- bin/network_service.dart
+++ bin/network_service.dart
@@ -1,4 +1,3 @@
-import 'package:network_service/model/album_model.dart';
 import 'package:network_service/model/post_model.dart';
 import 'package:network_service/services/http_service.dart';
 
@@ -11,7 +10,6 @@ void main() async {
   // String body = await NetworkService.GET(NetworkService.apiAlbums, NetworkService.headers);
   // List<Album> albums = NetworkService.parseAlbumList(body);
   // print(albums);
-
   Post post = Post(id: 12, userId: 78, title: "title", body: "body");
   String response = await NetworkService.POST(NetworkService.apiPosts, NetworkService.headers, post.toJson());
   print(response);
--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -40,6 +40,36 @@ class NetworkService {
     }
   }
 
+  static Future<String> PUT(String api, Map<String, String> headers, Map<String, dynamic> body) async{
+    Uri uri = Uri.https(baseUrl, api);
+    Response response = await put(uri, headers: headers, body: jsonEncode(body));
+    if(response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    } else {
+      throw Exception("Error");
+    }
+  }
+
+  static Future<String> PATCH(String api, Map<String, String> headers, Map<String, dynamic> body) async{
+    Uri uri = Uri.https(baseUrl, api);
+    Response response = await patch(uri, headers: headers, body: jsonEncode(body));
+    if(response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    } else {
+      throw Exception("Error");
+    }
+  }
+
+  static Future<String> DELETE(String api, Map<String, String> headers) async{
+    Uri uri = Uri.https(baseUrl, api);
+    Response response = await delete(uri, headers: headers);
+    if(response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    } else {
+      throw Exception("Error");
+    }
+  }
+
   // PARSING
   static List<Post> parsePostList(String body) {
     List json = jsonDecode(body);
